590a9582e607189333dcb386251d68443e1ddcaf
shahan953 <shahan953@hotmail.com>
2017-12-25 21:57:41 +0600

    db-con
    db

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,6 +1,24 @@
+var bodyParser = require('body-parser');
+var data = [{item: 'Hello'}, {item: 'Demo Text'}, {item: 'ABCD'}];
+var urlencodeParser = bodyParser.urlencoded({extended: false});
+var db = require('../config/db.js');
+
+
 var controller = function(app){
     app.get('/', function(req, res){
-        res.render('index', {msg : 'hello'});
+        res.render('index', {msg : data});
+    });
+
+    app.post('/',urlencodeParser, function(req, res){
+        data.push(req.body);
+        res.json(data);
+    });
+
+    app.delete('/:item', function(req, res){
+        data = data.filter(function(todo){
+            return todo.item.replace(/ /g, '-') !== req.params.item; 
+        });
+        res.json(data);
     });
 };
 
